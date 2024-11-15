ab162fbe859b8e10d0e18b9f1a66acd64c30efaf
aleksandarRa <radovic.aleksandar@gmx.at>
2024-11-15 16:01:27 +0100

    feat(project): convert npz to midi with same filename

--- transformer-xl/npzToMidi.py
+++ transformer-xl/npzToMidi.py
@@ -1,18 +1,19 @@
+import os
 import pathlib
-import numpy as np
-import csv
 from midi_parser import MIDI_parser
 from utils import get_quant_time
 import config_music as config
+import os
+import numpy as np
 
 # Initialize MIDI parser and get list of npz files
 idx_to_time = get_quant_time()
 midi_parser = MIDI_parser.build_from_config(config, idx_to_time)
 npz_filenames = list(pathlib.Path("data/npz_temp").rglob('*.npz'))
+file_names_without_extension = [file.stem for file in npz_filenames]
+file_names_array = np.array(file_names_without_extension)
 
-midi_filenames = [str(i) for i in range(1, len(npz_filenames)+1)]
-
-midi_filenames = [f + '.midi' for f in midi_filenames]
+midi_filenames = [f + '.midi' for f in file_names_array]
 
 sounds, deltas = zip(*[midi_parser.load_features(filename)
                        for filename in npz_filenames])
