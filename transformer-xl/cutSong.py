c4b479455cf7597e037feb9091db13cb940af37a
aleksandarRa <radovic.aleksandar@gmx.at>
2024-11-18 14:41:13 +0100

    feat(project): add code to cut song

--- transformer-xl/cutSong.py
+++ transformer-xl/cutSong.py
@@ -9,17 +9,23 @@ import numpy as np
 # Initialize MIDI parser and get list of npz files
 idx_to_time = get_quant_time()
 midi_parser = MIDI_parser.build_from_config(config, idx_to_time)
-npz_filenames = list(pathlib.Path("data/npz").rglob('*.npz'))
+
+npz_filenames = list(pathlib.Path("data/npz").rglob('0.npz'))
 file_names_without_extension = [file.stem for file in npz_filenames]
 file_names_array = np.array(file_names_without_extension)
 
-midi_filenames = [f + '.midi' for f in file_names_array]
+start = 1500
+end = 3000
+midi_filenames = [f +'npz_'+ str(start) + '-'+str(end) + '.midi' for f in file_names_array]
 
 sounds, deltas = zip(*[midi_parser.load_features(filename)
                        for filename in npz_filenames])
+
+sounds = (sounds[0][start:end],)
+deltas = (deltas[0][start:end],)
 
 midi_list = [midi_parser.features_to_midi(
     sound, delta) for sound, delta in zip(sounds, deltas)]
 
 for midi, filename in zip(midi_list, midi_filenames):
-    midi.save("data/midi_temp/"+filename)
+    midi.save("data/cutted_midi/"+filename)
